01cd97033e13c75545cfa99869daa4dbcc4d81d8
Paul Englert <mail@paulenglert.de>
2020-01-25 13:17:01 +0000

    update to use es6 code and babeling it; add temperature placeholder for api

---
+++ api/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+import temperatureRouter from "./temperature";
+
+
+// setup app
+const app = express();
+app.use(express.json());
+
+
+// configure apis
+const apis = [
+	["/temperature", temperatureRouter],
+];
+
+const globalRouter = express.Router();
+globalRouter.get("/", (req, res) => res.json({version:"0.0.0",apis:apis.map(a => a[0])}));
+
+apis.map(api => app.use(...api));
+app.use("/", globalRouter);
+
+
+// start server
+const port = process.env.PORT || 36000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+export default app;
--- app.js
+++ app.js
@@ -1,7 +1,2 @@
-const express = require('express')
-const app = express()
-const port = 36000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+require("babel-register");
+require("./api/app");
