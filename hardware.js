8404d2212a565771a8ee6f1eb947f0e054747e9a
Paul Englert <mail@paulenglert.de>
2020-01-26 11:41:13 +0000

    update to correct pin config

--- hardware.js
+++ hardware.js
@@ -30,18 +30,18 @@ const servos = [
       power: 'P9_5',
       ground: 'P9_1'
     }
-  }//,
-  // {
-  //   dutyCycle: {
-  //     min: 0.046, // 0.043
-  //     max: 0.134  // 0.136
-  //   },
-  //   pins: {
-  //     signal: 'P9_16',
-  //     power: 'P9_8',
-  //     ground: 'P9_2'
-  //   }
-  // }
+  },
+  {
+    dutyCycle: {
+      min: 0.046, // 0.043
+      max: 0.134  // 0.136
+    },
+    pins: {
+      signal: 'P8_19',
+      power: 'P9_8',
+      ground: 'P8_1'
+    }
+  }
 ];
 
 module.exports = {temperatureSensors, servos};
